df1a4719a75f414547c60f4c8c8dcfd512f55845
Aya berjawi <ayabarjawi@gmail.com>
2021-01-04 13:41:54 +0200

    new

--- js/app.js
+++ js/app.js
@@ -44,44 +44,5 @@ var lima = {
 }
  var locations = [seattle,tokyo,dubai,paris,lima];
 
- for(var i=0;i<locations.length;i++){
-     cookiesperHour(locations[i]);
- }
+
  
-
-
- function cookiesperHour(obj){
-     var totalCookies=0;
-     var purshasedCookie=[]
-     var time='am'
-    for (var i=6;i<=19;i++){
-        var timing = i <=12? i:i-12;
-        if(timing>=12)time='pm';
-        var computedCookies=computedCookies1(getRandomInt(5,70),obj.avgCookieCust);
-        totalCookies+=computedCookies;
-        purshasedCookie[i-6]=timing+time+": "+computedCookies+" cookies";
-    }
-    purshasedCookie[purshasedCookie.length]="total is "+totalCookies;
-    obj.result=purshasedCookie;
- }
- 
- function computedCookies1(hour1,avg){
-    return Math.round(hour1*avg);
- }
-
- for(var i=0;i<locations.length;i++){
-    displayAmoutOfCookies(locations[i]);
-}
- function displayAmoutOfCookies(obj){
-     var parent=document.getElementById("salesList");
-
-     var ul=document.createElement('ul');
-     ul.innerHTML="<h2>"+obj.name+"</h2>";
-     for(var i=0;i<obj.result.length;i++){
-        var li=document.createElement('li');
-        li.textContent=obj.result[i];
-
-        ul.append(li);
-     }
-     parent.append(ul);
- }
