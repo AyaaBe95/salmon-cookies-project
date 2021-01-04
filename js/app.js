b196b3ed1f6c7d32e6a4bf1d94f14784d3d3f328
Aya berjawi <ayabarjawi@gmail.com>
2021-01-04 13:56:01 +0200

    neww

--- js/app.js
+++ js/app.js
@@ -8,38 +8,38 @@ function getRandomInt(min, max) {
 
 var seattle = {
     name:"Seattle",
-    minHourlyCust: 20,
-    maxHourlyCust: 70,
-    avgCookieCust: 6.3,
+    minHour: 20,
+    maxHour: 70,
+    avg: 6.3,
     result:[],
     
 }
 var tokyo = {
     name:"Tokyo",
-    minHourlyCust: 3,
-    maxHourlyCust: 24,
-    avgCookieCust: 1.2,
+    minHour: 3,
+    maxHour: 24,
+    avg: 1.2,
     result:[],
 }
 var dubai = {
     name: "Dubi",
-    minHourlyCust: 11,
-    maxHourlyCust: 38,
-    avgCookieCust: 3.8,
+    minHour: 11,
+    maxHour: 38,
+    avg: 3.8,
     result:[],
 }
 var paris = {
     name:"Paris",
-    minHourlyCust: 20,
-    maxHourlyCust: 38,
-    avgCookieCust: 2.3,
+    minHour: 20,
+    maxHour: 38,
+    avg: 2.3,
     result:[],
 }
 var lima = {
     name:"Lima",
-    minHourlyCust: 2,
-    maxHourlyCust: 16,
-    avgCookieCust: 4.6,
+    minHour: 2,
+    maxHour: 16,
+    avg: 4.6,
     result:[],
 }
  var locations = [seattle,tokyo,dubai,paris,lima];
