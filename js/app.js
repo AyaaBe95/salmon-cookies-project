9a5b63ab0c44d5bebaafcd2d673b49ca0c428fc2
Aya berjawi <ayabarjawi@gmail.com>
2021-01-05 17:34:38 +0200

    lab07

--- js/app.js
+++ js/app.js
@@ -10,17 +10,6 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function calcCustAvg() {
-
-    for (let index = 0; index < hours.length; index++) {
-    
-        var avg = Math.floor(getRandomInt(this.minHourlyCust, this.maxHourlyCust) * this.avgCookieCust);
-        var total = total + avg
-        this.result.push(avg)
-
-
-    }
-}
 
     // construction
 
@@ -29,17 +18,39 @@ function calcCustAvg() {
         this.minHourlyCust = minHourlyCust;
         this.maxHourlyCust = maxHourlyCust;
         this.avgCookieCust = avgCookieCust;
-        result = [];
-        total = 0;
+        this.result = [];
+        this.total = 0;
 
     }
 
+    Shop.prototype.calcCustAvg = function () {
+
+        for (let index = 0; index < hours.length; index++) {
+        
+            var avg = Math.floor(getRandomInt(this.minHourlyCust, this.maxHourlyCust) * this.avgCookieCust);
+            var total = total + avg
+            this.result.push(avg)
+    
+    
+        }
+    }
+
+    
+
     // call
-    var seattle = new shop("seattle", 23, 65, 6.3);
-    var dubai = new shop("dubai", 11, 38, 3.7);
-    var tokyo = new shop("tokyo", 3, 24, 1.2);
-    var paris = new shop("paris", 20, 38, 2.3);
-    var lima = new shop("lima", 2, 16, 4.6);
+    var seattle = new Shop("seattle", 23, 65, 6.3);
+    var dubai = new Shop("dubai", 11, 38, 3.7);
+    var tokyo = new Shop("tokyo", 3, 24, 1.2);
+    var paris = new Shop("paris", 20, 38, 2.3);
+    var lima = new Shop("lima", 2, 16, 4.6);
+
+    seattle.calcCustAvg();
+    dubai.calcCustAvg();
+    tokyo.calcCustAvg();
+    paris.calcCustAvg();
+    lima.calcCustAvg();
+        
+
 
     // DOM header
 
@@ -64,19 +75,32 @@ Shop.prototype.render = function () {
     var tableRow = document.createElement('tr');
     table.appendChild(tableRow);
 
+
     var tableContent = document.createElement('td');
     tableContent.textContent = this.name;
     tableRow.appendChild(tableContent);
 
-    for (let index = 0; index < locations.length; index++) {
-        var tableContent = document.createElement('td');
-        tableContent.textContent=this.result[i]
-        tableContent.appendChild(td)
+    for (let index = 0; index < hours.length; index++) {
+        var td = document.createElement('td');
+        
+        td.textContent=this.result[index]
+        tableRow.appendChild(td)
 
         
     }
 
 }
+
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+
+
+
+
+
 
 // DOM Footer
 Shop.prototype.renderfooter = function () {
